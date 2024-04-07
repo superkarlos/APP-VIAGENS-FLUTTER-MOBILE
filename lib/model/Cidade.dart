bd1d57285de342f9e57cd883f5f00c8ebae561bc
Pedro Henrique Bastos Viana <143200652+phViana-Dev@users.noreply.github.com>
2024-04-06 21:27:14 -0300

    telaCadastro criada

---
+++ lib/model/Cidade.dart
@@ -0,0 +1,9 @@
+class Cidade{
+  String name;
+  String url;
+
+  Cidade(this.name, this.url);
+
+  String get _url => this.url;
+  
+}
---
+++ lib/model/Usuario.dart
@@ -0,0 +1,10 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Usuario {
+  String nome;
+  int idade;
+  int saldo;
+
+  Usuario(this.nome, this.idade, this.saldo);
+}
