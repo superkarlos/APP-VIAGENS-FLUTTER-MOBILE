5e491c8aae0193b33fda18f89ea5d08576988469
Matheus Queiroz <137003929+matheusqap@users.noreply.github.com>
2024-04-09 12:17:33 -0300

    Update tela_de_login.dart

--- app/lib/screens/tela_de_login.dart
+++ app/lib/screens/tela_de_login.dart
@@ -3,10 +3,7 @@ import 'package:My_App/model/Usuario.dart';
 import 'package:My_App/screens/tela_principal.dart';
 
 class TelaLogin extends StatefulWidget {
-  const TelaLogin({super.key});
-
   @override
-  // ignore: library_private_types_in_public_api
   _TelaLoginState createState() => _TelaLoginState();
 }
 
@@ -14,88 +11,94 @@ class _TelaLoginState extends State<TelaLogin> {
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
 
-   bool _obscureText = true;
+  bool _obscureText = true;
 
-void _entrar() async {
-  if (_loginController.text.isEmpty || _senhaController.text.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Por favor, preencha todos os campos.'),
-        duration: Duration(seconds: 2),
-      ),
-    );
-    return;
-  }
+  void _entrar() async {
+    if (_loginController.text.isEmpty || _senhaController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Por favor, preencha todos os campos.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      return;
+    }
 
-  List<Usuario> usuarios = await Usuario.carregarUsuarios();
+    List<Usuario> usuarios = await Usuario.carregarUsuarios();
 
-  if (usuarios.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Login ou senha incorretos. Por favor, tente novamente.'),
-        duration: Duration(seconds: 2),
-      ),
-    );
-    return;
-  }
+    if (usuarios.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+              Text('Login ou senha incorretos. Por favor, tente novamente.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      return;
+    }
 
-  Usuario? usuarioEncontrado;
+    Usuario? usuarioEncontrado;
 
-  for (var usuario in usuarios) {
-    if (usuario.nomeUsuario == _loginController.text && usuario.senha == _senhaController.text) {
-      usuarioEncontrado = usuario;
-      break;
+    for (var usuario in usuarios) {
+      if (usuario.nomeUsuario == _loginController.text &&
+          usuario.senha == _senhaController.text) {
+        usuarioEncontrado = usuario;
+        break;
+      }
+    }
+
+    if (usuarioEncontrado != null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => TelaPrincipal(usuarioEncontrado!),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+              Text('Login ou senha incorretos. Por favor, tente novamente.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      return;
     }
   }
 
-  if (usuarioEncontrado != null) {
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (context) => TelaPrincipal(usuarioEncontrado!),
-      ),
-    );
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Login ou senha incorretos. Por favor, tente novamente.'),
-        duration: Duration(seconds: 2),
-      ),
-    );
-    return;
-  }
-}
-
-
-  void _cadastrarUsuario(){
-      Navigator.of(context).pushNamed('/screens/TelaCadastro.dart');
+  void _cadastrarUsuario() {
+    Navigator.of(context).pushNamed('/screens/TelaCadastro.dart');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 201, 231, 255),
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              width: 150,
-              height: 150,
-              child: Image.asset("assets/images/flutter.png"),
-            ),
-            const SizedBox(height: 70,),
-            TextField(
-              controller: _loginController,
-              decoration: const InputDecoration(labelText: 'Nome de usário'),
-            ),
-            TextField(
-              controller: _senhaController,
-               obscureText: _obscureText,
+      backgroundColor: Color.fromARGB(255, 201, 231, 255),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: Image.asset("assets/images/flutter.png"),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              TextField(
+                controller: _loginController,
+                decoration: InputDecoration(labelText: 'Nome de usuário'),
+              ),
+              TextField(
+                controller: _senhaController,
+                obscureText: _obscureText,
                 decoration: InputDecoration(
                   labelText: 'Senha',
                   suffixIcon: IconButton(
-                    icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                    icon: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off),
                     onPressed: () {
                       setState(() {
                         _obscureText = !_obscureText;
@@ -103,30 +106,30 @@ void _entrar() async {
                     },
                   ),
                 ),
-            ),
-            const SizedBox(height: 15),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: _entrar,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 199, 229, 253),
-                    foregroundColor: Colors.purple,
+              ),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: _entrar,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 199, 229, 253),
+                      foregroundColor: Colors.purple,
+                    ),
+                    child: Text('Entrar'),
                   ),
-                  child: const Text('Entrar'),
-                ),
-                const Spacer(),
-                ElevatedButton(
-                  onPressed: _cadastrarUsuario,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 199, 229, 253),
-                    foregroundColor: Colors.purple,
-                  ),
-                  child: const Text('Cadastrar-se')
-                ),
-              ],
-            ),
-          ],
+                  Spacer(),
+                  ElevatedButton(
+                      onPressed: _cadastrarUsuario,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 199, 229, 253),
+                        foregroundColor: Colors.purple,
+                      ),
+                      child: Text('Cadastrar-se')),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
