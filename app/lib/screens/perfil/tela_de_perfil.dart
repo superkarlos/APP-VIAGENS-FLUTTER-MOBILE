110d42b7076cd46c84ca1dbd75320b2b1bcf1cea
Lucas Emanuel <lerc.rj@hotmail.com>
2024-05-26 16:55:00 -0300

    Criação da tela de perfil

---
+++ app/lib/screens/perfil/tela_de_perfil.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:My_App/model/usuario.dart';
+
+class TelaDePerfil extends StatelessWidget {
+  final Usuario usuario;
+
+  const TelaDePerfil({Key? key, required this.usuario}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('assets/images/pngwing.com.png'),
+              ),
+              SizedBox(height: 16),
+              Text(
+                usuario.nome,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Saldo: \$${usuario.saldo.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Lógica para editar as informações do usuário
+                },
+                child: Text(
+                  'Editar Informações'
+                ),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, 
+                  backgroundColor: Colors.green, // Define a cor do texto do botão
+                  shape: RoundedRectangleBorder( // Define a forma do botão
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12), // Define o padding do botão
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
