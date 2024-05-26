a86a1c5d40e1368453da020970ad80b111cc19d8
Matheus Queiroz <matheusqap@gmail.com>
2024-05-25 21:35:46 -0300

    main e firebase_options

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,17 +4,23 @@ import 'package:My_App/screens/tela_de_login.dart';
 import 'package:My_App/service/destino_service.dart';
 import 'package:My_App/utils/routes.dart';
 import 'package:flutter/material.dart';
-//import 'firebase_option.dart';
+import 'firebase_option.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 //import 'package:My_App/screens/tela_de_cadastro.dart';
 //import 'package:My_App/screens/tela_de_login.dart';
 
-void main() => runApp(const MeuApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MeuApp());
+}
 
 class MeuApp extends StatelessWidget {
-  const MeuApp({super.key}); 
+  const MeuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
